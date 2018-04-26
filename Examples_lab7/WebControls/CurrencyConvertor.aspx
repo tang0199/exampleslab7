a75411f3631f479421859b26c599f24af9668ba0
Wenjia Tang <31903891+tang0199@users.noreply.github.com>
2018-04-25 20:31:50 -0400

    Add files via upload

---
+++ Examples_lab7/WebControls/CurrencyConvertor.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"
+    CodeFile="CurrencyConvertor.aspx.cs" Inherits="CurrencyConvertor" %>
+
+<!DOCTYPE html>
+
+<html>
+  <head runat="server">
+    <title>Currency Converter</title>
+  </head>
+  <body>
+    <form ID="Form1" method="post" runat="server">
+
+        <div style="border-right: thin ridge; padding-right: 20px; border-top: thin ridge;
+            padding-left: 20px; padding-bottom: 20px; border-left: thin ridge; width: 531px;
+            padding-top: 20px; border-bottom: thin ridge; font-family: Verdana; background-color: #FFFFE8">
+           
+            Convert: &nbsp;
+            <asp:TextBox ID="txtCanadianDollar" runat="server" style="width: 102px" />&nbsp; Canadian dollars to &nbsp;
+            <asp:DropdownList ID="drpCurrency" runat="server" />
+            <br /><br />
+            <asp:Button text="submit" ID="btnSubmit" runat="server" OnClick="btnSubmit_Click" />
+            <br /><br />
+            <asp:Literal ID="ltrResult" runat="server" />
+        
+        </div>
+    </form>
+  </body>
+</html>
---
+++ Examples_lab7/WebControls/CurrencyConvertor.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class CurrencyConvertor : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (this.IsPostBack == false)
+        {
+            drpCurrency.Items.Add(new ListItem("US Dollar", "0.9"));
+            drpCurrency.Items.Add(new ListItem("Euros", "0.75"));
+            drpCurrency.Items.Add(new ListItem("Japanese Yen", "110.33"));
+            drpCurrency.Items.Add(new ListItem("Chinese RMB", "5.2"));
+        }		
+    }
+
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        decimal oldAmount;
+        
+        bool success = Decimal.TryParse(txtCanadianDollar.Text, out oldAmount);
+
+        if (success)
+        {
+            // Retrieve the selected ListItem object by its index number.
+            //ListItem item = drpCurrency.Items[drpCurrency.SelectedIndex];
+
+            // Retrieve the selected ListItem object by SelectedItem property.
+            ListItem item = drpCurrency.SelectedItem; 
+
+            decimal newAmount = oldAmount * Decimal.Parse(item.Value);
+            
+            ltrResult.Text = oldAmount.ToString() + " Canadian dollars = ";
+            ltrResult.Text += newAmount.ToString() + " " + item.Text;
+        }
+        else
+        {
+            ltrResult.Text = "The number you typed in was not in the correct format. ";
+            ltrResult.Text += "Use only numbers.";
+        }
+    }
+}
